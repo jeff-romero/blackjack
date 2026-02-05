7a047778a7b0ad1a5c78c137f5faf5533f0a5e6c
jeff-romero <jeffromero86@gmail.com>
2026-02-05 15:10:11 -0800

    setup dealer and player space

---
+++ blackjack.js
@@ -0,0 +1,27 @@
+/*
+player:
+    hit - take another card
+    stand/stay - take no more cards
+    double down - increase initial bet by 100% and take one more card. the additional bet is placed next to the original bet.
+    split - create two hands from a starting hand where both cards have the same value. each new hand gets a second card resulting in two starting hands. this requires an additional bet on the second hand. the two hands are played out independently, and the wager on each hand is won or lost independently.
+    surrender - forfeit half the bet and end the hand immediately. not allowed after splitting.
+*/
+
+
+class Chip {
+    constructor() {
+
+    }
+}
+
+class Player {
+    constructor() {
+
+    }
+}
+
+class Dealer{
+    constructor() {
+
+    }
+}
